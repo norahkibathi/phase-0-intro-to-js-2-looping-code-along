356fd4e7b37215494222d7534cf6e445e01edb48
norahkibathi <norah.njeri@student.moringaschool.com>
2024-01-19 06:38:48 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+  }
+    function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
